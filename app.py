17bfef472710790903f70399104e26df7322cefa
nikitadpopel <nikita.d.popel@gmail.com>
2020-12-23 11:08:39 -0500

    edited readme.md, app.py, and scraper.[y'

--- app.py
+++ app.py
@@ -1,93 +1,78 @@
+# This was created by Danila Popel and Nikita Popel. This project was originally created on December 20th, 2020.
+
 from bs4 import BeautifulSoup
 import urllib.request, os, platform, time, csv, requests
+from datetime import datetime, timedelta
 from scraper import *
 
-import matplotlib.pyplot as plt
-
-def inputDate(dateprompt,command):
-    dateloop = 1
-    while dateloop == 1:
-        userinput_date = input(dateprompt)
-        if len(userinput_date) == 10:
-            if userinput_date[4] == '-' and userinput_date[-3] == '-':
-                try:
-                    int(userinput_date[0:3])
-                    int(userinput_date[5:6])
-                    int(userinput_date[8:9])
-                    command
-                    dateloop = 0
-                except:
-                    print('Date Error: Make sure characters between Hyphens are integers!')
-            else:
-                print('Date Error: Make sure you format date using Hyphens!')
-        else:
-            print('Date Error: Character count too low; expected 10, but recieved', len(userinput_date))
-
-def isDate(userinput_date):
-    if len(userinput_date) == 10:
-        if userinput_date[4] == '-' and userinput_date[-3] == '-':
-            try:
-                int(userinput_date[0:3])
-                int(userinput_date[5:6])
-                int(userinput_date[8:9])
-                return True
-            except:
-                print('Date Error: Make sure characters between Hyphens are integers!')
-        else:
-            print('Date Error: Make sure you format date using Hyphens!')
-    else:
-        print('Date Error: Character count too low; expected 10, but recieved', len(userinput_date))
-
 if __name__ == '__main__':
-    prompt_inputstock = 'Enter the stock that you would like to look up: '
-    userstocksymbol = input(f'\n*********************************\n* Welcome to the Stonk Scraper! *\n*********************************\n{prompt_inputstock}').upper()
-    loop = 1
-    while loop == 1:
+    prompt_inputstock = 'Enter the stock that you would like to look up?\n> '
+    print('\n*********************************\n* Welcome to the Stonk Scraper! *\n*********************************')
+    introloop = 1
+    while introloop == 1:
+        userstocksymbol = input(prompt_inputstock).upper()
+        if checkExist(userstocksymbol) == True:
+            introloop = 0
+        else:
+            print('You entry is not a valid stock symbol.')
+    mainloop = 1
+    while mainloop == 1:
         currstock = Stock(0,userstocksymbol)
         try:
-            currentstate_line1 = 'Date: ' + str(currstock.getCurrent()[0]) + '\tOpening Price($): ' + str(round(float(currstock.getCurrent()[1]),2)) + '\tClosing Price($): ' + str(round(float(currstock.getCurrent()[4]),2)) + '\t|'
-            currentstate_line2 = 'Daily Low($): ' + str(round(float(currstock.getCurrent()[3]),2)) + '\tDaily High($): ' + str(round(float(currstock.getCurrent()[2]),2)) + '\t\tDaily Volume: ' + str(round(float(currstock.getCurrent()[6]),2)) + '\t|'
-            print('\n\t\t\t' + ('-' * (len(currstock.getName())+4))+'\n\t\t\t| '+ currstock.getName() + ' |')
-            print(('-' * len(currentstate_line1)) + '\n' + currentstate_line1 + '\n' + currentstate_line2 + '\n' + ('-' * len(currentstate_line1)))
-
-
-            plt.plot(currstock.getCloses())
-            plt.show()
+            date_string = 'Date: ' + str(currstock.getCurrent()[0]) + ' ' * (40 - len('Date: ' + str(currstock.getCurrent()[0])))
+            opening_string = 'Opening Price($): ' + str(round(float(currstock.getCurrent()[1]),2)) + ' ' * (40 - len('Opening Price($): ' + str(round(float(currstock.getCurrent()[1]),2))))
+            closing_string = 'Closing Price($): ' + str(round(float(currstock.getCurrent()[4]),2)) + ' ' * (40 - len('Closing Price($): ' + str(round(float(currstock.getCurrent()[4]),2))))
+            low_string = 'Daily Low($): ' + str(round(float(currstock.getCurrent()[3]),2)) + ' ' * (40 - len('Daily Low($): ' + str(round(float(currstock.getCurrent()[3]),2))))
+            high_string = 'Daily High($): ' + str(round(float(currstock.getCurrent()[2]),2)) + ' ' * (40 - len('Daily High($): ' + str(round(float(currstock.getCurrent()[2]),2))))
+            volume_string = 'Daily Volume: ' + str(round(float(currstock.getCurrent()[6]),2)) + ' ' * (40 - len('Daily Volume: ' + str(round(float(currstock.getCurrent()[6]),2))))
+            name_string = '| ' + currstock.getName() + ' |'
+            out_line1 = '| ' + date_string + opening_string + closing_string + ' |'
+            out_line2 = '| ' + low_string + high_string + volume_string + ' |'
+            namelen = len(currstock.getName())
+            print('\n' + (' ' * int((120 - namelen) / 2)) + '+' + ('-' * (namelen + 2)) + '+\n' + (' ' * int((120 - namelen) / 2)) + name_string)
+            print('+' + ('-' * (int((120 - namelen) / 2)-1)) + '+' + ('-' * (namelen + 2)) + '+' + ('-' * (120 - (int((120 - namelen) / 2) - 1) - (namelen + 2))) + '+\n' + out_line1 + '\n' + out_line2 + '\n+' + ('-' * (len(out_line1)-2)) + '+')
         except:
             pass
-        userinput = input('What would you like to do next?\n[S]earch by Date\t[C]ompare over Time\t[N]ew Stock\t[Q]uit\n> ').upper()
-        if userinput == 'S':
-            """
-            dateloop = 1
-            while dateloop == 1:
-                userinput_date = input('Input your desired date (Format: YYYY-MM-DD): ')
-                if isDate(userinput_date) == True:
-                    for i in currstock.getHistory():
-                        if userinput_date in i[0]:
-                            print(i)
+        stockloop = 1
+        while stockloop == 1:
+            userinput = input('\nWhat would you like to do next?\n[S]earch by Date\t[C]ompare over Time\t[N]ew Stock\t[Q]uit\n> ').upper()
+            if userinput == 'S':
+                dateloop = 1
+                while dateloop == 1:
+                    userinput_date = input('Input your desired date (Format: YYYY-MM-DD): ')
+                    if isDate(userinput_date) == True:
+                        print(currstock.fixDate(userinput_date))
+                        dateloop = 0
+            elif userinput == 'C':
+                print('Input the dates in chronological order (Start Date first, then more Latest Date).')
+                dateloop = 1
+                while dateloop == 1:
+                    userinput_date1 = input('Start Date (Format: YYYY-MM-DD): ')
+                    if isDate(userinput_date1) == True:
+                        userinput_date1 = currstock.fixDate(userinput_date1)[0]
+                        dateloop = 0
+                        """
+                        if isDate(userinput_date1) == True:
                             dateloop = 0
-            """
-            dateprompt = 'Input your desired date (Format: YYYY-MM-DD): '
-            userinput_date = input(dateprompt)
-            inputDate(dateprompt,[print(i) for i in currstock.getHistory() if userinput_date in i[0]])
-
-        elif userinput == 'C':
-            print('Input the dates in chronological order')
-            dateloop = 1
-            while dateloop == 1:
-                userinput_date1 = input('Date #1 (Format: YYYY-MM-DD): ')
-                if isDate(userinput_date1) == True:
-                    dateloop = 0
-            dateloop = 1
-            while dateloop == 1:
-                userinput_date2 = input('Date #2 (Format: YYYY-MM-DD): ')
-                if isDate(userinput_date2) == True:
-                    dateloop = 0
-            print(currstock.compareStock(userinput_date1,userinput_date2))
-        elif userinput == 'N':
-            userstocksymbol = input(prompt_inputstock).upper()
-        elif userinput == 'Q':
-            print('Quitting!')
-            loop = 0
-        else:
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n! Try Again. That was not one of the options! !\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                            """
+                dateloop = 1
+                while dateloop == 1:
+                    userinput_date2 = input('Latest Date (Format: YYYY-MM-DD): ')
+                    if isDate(userinput_date2) == True:
+                        userinput_date2 = currstock.fixDate(userinput_date2)[0]
+                        dateloop = 0
+                        """
+                        if isDate(userinput_date2) == True:
+                            dateloop = 0
+                            """
+                print(currstock.compareStock(userinput_date1,userinput_date2))
+            elif userinput == 'N':
+                userstocksymbol = input(prompt_inputstock).upper()
+                stockloop = 0
+            elif userinput == 'Q':
+                print('Quitting!')
+                stockloop = 0
+            else:
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n! Try Again. That was not one of the options! !\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        if userinput == 'Q':
+            mainloop = 0
--- scraper.py
+++ scraper.py
@@ -1,9 +1,14 @@
+# This was created by Danila Popel and Nikita Popel. This project was originally created on December 20th, 2020.
+
 from bs4 import BeautifulSoup
 import urllib.request, os, platform, time, csv, requests
+from datetime import datetime, timedelta
+
 # Below is the function that checks whether or not the stock symbol that the user
 # inputted is a valid stock symbol recognized by the NASDAQ, NYSE, and other
 # exchanges
-def checkexist(userstocksymbol):
+
+def checkExist(userstocksymbol):
     #tickersymbols = [i[1] for i in [i.split('|') for i in ''.join([i.decode('utf-8') for i in urllib.request.urlopen('ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqtraded.txt')]).split('\n')][1:-2]]
     tickersymbols = []
     for i in urllib.request.urlopen('ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqtraded.txt'):
@@ -13,21 +18,38 @@ def checkexist(userstocksymbol):
     else:
         return False
 
-# Below is the function that creates our Stock Class
+def isDate(userinput_date):
+    if len(userinput_date) == 10:
+        if userinput_date[4] == '-' and userinput_date[-3] == '-':
+            try:
+                int(userinput_date[0:3])
+                int(userinput_date[5:6])
+                int(userinput_date[8:9])
+                return True
+            except:
+                print('Date Error: Make sure characters between Hyphens are integers!')
+        else:
+            print('Date Error: Make sure you format date using Hyphens!')
+    else:
+        print('Date Error: Character count too low; expected 10, but recieved', len(userinput_date))
+def decrementDate(userinput_date):
+    return (datetime.strptime(userinput_date, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d')
+# Below is the definition of our stock class
 class Stock:
+    # This is the instantiation of our Stock
     def __init__(self, history, userstocksymbol):
         self.symbol = userstocksymbol
-        if checkexist(self.symbol) == True:
-            req = requests.get(f'https://query1.finance.yahoo.com/v7/finance/download/{userstocksymbol}?period1=0&period2=9999999999&interval=1d&events=history&includeAdjustedClose=true')
-            self.history = req.content.decode('utf-8').split('\n')
-            self.history = [i.split(',') for i in req.content.decode('utf-8').split('\n')][1:]
-            currentstockurl = 'https://finance.yahoo.com/quote/' + self.symbol
-            currentpage = requests.get(currentstockurl)
-            soup = BeautifulSoup(currentpage.content, 'html5lib')
-            mydivs = soup.findAll('h1', {'data-reactid' : '7'})
-            self.name = str(str(mydivs[0]).split('>')[1]).split('<')[0]
-        else:
-            print('This shit does not exist, GTFO.')
+        #if checkExist(self.symbol) == True:
+        req = requests.get(f'https://query1.finance.yahoo.com/v7/finance/download/{userstocksymbol}?period1=0&period2=9999999999&interval=1d&events=history&includeAdjustedClose=true')
+        self.history = req.content.decode('utf-8').split('\n')
+        self.history = [i.split(',') for i in req.content.decode('utf-8').split('\n')][1:]
+        currentstockurl = 'https://finance.yahoo.com/quote/' + self.symbol
+        currentpage = requests.get(currentstockurl)
+        soup = BeautifulSoup(currentpage.content, 'html5lib')
+        mydivs = soup.findAll('h1', {'data-reactid' : '7'})
+        self.name = str(str(mydivs[0]).split('>')[1]).split('<')[0]
+        """else:
+            print('You entry is not a valid stock symbol.')"""
     def getHistory(self):
         return self.history
     def getSymbol(self):
@@ -53,6 +75,7 @@ class Stock:
                 date1_index = n
             if date2 in i[0]:
                 date2_index = n
+
         if float(self.history[date1_index][4]) < float(self.history[date2_index][4]):
             isGain = True
             output = 'the percentage change since ' + str(date1) + ' is +'
@@ -62,3 +85,22 @@ class Stock:
         percentage_change = ((float(self.history[date1_index][4]) - float(self.history[date2_index][4])) / float(self.history[date1_index][4])) * (-100)
         output += str(round(percentage_change, 2)) + '%'
         return output
+    def checkDate(self, userinput_date):
+        for i in self.getHistory():
+            if userinput_date in i:
+                return True
+        return False
+    def fixDate(self, userinput_date):
+        #currstock.fixDate()
+        fixloop = 1
+        datechanged = 0
+        while fixloop == 1:
+            if self.checkDate(userinput_date) == True:
+                fixloop = 0
+            elif self.checkDate(userinput_date) == False:
+                userinput_date = decrementDate(userinput_date)
+                #userinput_date = f'{userinput_date[0:8]}{int(userinput_date[8:10])-1}'
+                datechanged = 1
+        if datechanged == 1:
+            print('Using results for closest date (' + userinput_date + ').')
+        return [i for i in self.getHistory() if userinput_date in i][0]
